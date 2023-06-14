b121476fb01866aa3e49fb1a0b7d45ab5dd361f1
Conan Wang <shanxipcs@aliyun.com>
2023-06-14 16:19:10 +0100

    Amend codes for day2
    Amend the codes for day2 according to comments

--- 1_Introduciton to Python GUI/Python_GUI_day1.py
+++ 1_Introduciton to Python GUI/Python_GUI_day1.py
@@ -3,86 +3,91 @@
 import dearpygui.dearpygui as dpg
 import math
 
-# All dearpygui apps start with create_context()
-dpg.create_context()
-# The viewport is the window created by the operating system to display the GUI. 
-# The viewport is created using the create_viewport() function
-dpg.create_viewport(title='demonstration of dearpygui widgets', width=1000, height=700)
+def start_GUI():
+    # All dearpygui apps start with create_context()
+    dpg.create_context()
+    # The viewport is the window created by the operating system to display the GUI. 
+    # The viewport is created using the create_viewport() function
+    dpg.create_viewport(title='demonstration of dearpygui widgets', width=1000, height=700)
 
-# The following codes generate data for plotting
-sindatax = []
-sindatay = []
-for i in range(0, 500):
-    sindatax.append(i / 1000)
-    sindatay.append(0.5 + 0.5 * math.sin(50 * i / 1000))
+    # The following codes generate data for plotting
+    sindatax = []
+    sindatay = []
+    for i in range(0, 500):
+        sindatax.append(i / 1000)
+        sindatay.append(0.5 + 0.5 * math.sin(50 * i / 1000))
 
-### Start creating GUI ###
+    ### Start creating GUI ###
 
-"""
-In Python, the with statement is used for working with objects that support a context manager protocol. 
-It ensures that resources are properly managed and cleaned up when they are no longer needed. Here,
-the with statement is used to create a window and all the widgets added to the window will be automatically
-added to the window. The same for when you create a menu bar and plot widget.
-"""
-
-# We first create a window using the window() function
-with dpg.window(label="Window1", width=700, height=700,pos=[0,0]):
-    # All the following widgets are added to Window1
-    dpg.add_text("Text widget")
-    dpg.add_button(label="Button widget")
-    dpg.add_input_text(label="Input text widget", default_value="Input text box widgets")
-    dpg.add_slider_float(label="Slider widget", default_value=0.273, max_value=1)
-    dpg.add_text("Text widget")
-    dpg.add_checkbox(label="Checkbox widget - option 1")
-    dpg.add_checkbox(label="Checkbox widget - option 2")
-    dpg.add_checkbox(label="Checkbox widget - option 3")
     """
-    For each widget, parameter pos=[x,y] (x is the horizontal position and y is the vertical position)
-    can be used to set the position of the widget. If not set, the widget will be placed automatically.
-    In this case, the radio button will be set below the checkbox widgets rather than side by side.
-    You can have a try by deleting the pos=[220,140] parameter.
+    In Python, the with statement is used for working with objects that support a context manager protocol. 
+    It ensures that resources are properly managed and cleaned up when they are no longer needed. Here,
+    the with statement is used to create a window and all the widgets added to the window will be automatically
+    added to the window. The same for when you create a menu bar and plot widget.
     """
-    dpg.add_radio_button(["Radio button widget - option 1", "Radio button widget - option 2", "Radio button widget - option 3"], horizontal=False,pos=[220,140])
-   
-    # The following codes create a plot widget
-    with dpg.plot(label="Line Series", height=300, width=400):
-    # optionally create legend
-        dpg.add_plot_legend()
-        # REQUIRED: create x and y axes
-        dpg.add_plot_axis(dpg.mvXAxis, label="x")
-        dpg.add_plot_axis(dpg.mvYAxis, label="y", tag="y_axis")
-        # series belong to a y axis
-        dpg.add_line_series(sindatax, sindatay, label="0.5 + 0.5 * sin(x)", parent="y_axis") 
 
-# There can be as many windows as you want in a viewport, here we generate a second window
-with dpg.window(label="Window2", width=700, height=700,pos=[700,0]):
-    dpg.add_text("Text widget")
+    # We first create a window using the window() function
+    with dpg.window(label="Window1", width=700, height=700,pos=[0,0]):
+        # All the following widgets are added to Window1
+        dpg.add_text("Text widget")
+        dpg.add_button(label="Button widget")
+        dpg.add_input_text(label="Input text widget", default_value="Input text box widgets")
+        dpg.add_slider_float(label="Slider widget", default_value=0.273, max_value=1)
+        dpg.add_text("Text widget")
+        dpg.add_checkbox(label="Checkbox widget - option 1")
+        dpg.add_checkbox(label="Checkbox widget - option 2")
+        dpg.add_checkbox(label="Checkbox widget - option 3")
+        """
+        For each widget, parameter pos=[x,y] (x is the horizontal position and y is the vertical position)
+        can be used to set the position of the widget. If not set, the widget will be placed automatically.
+        In this case, the radio button will be set below the checkbox widgets rather than side by side.
+        You can have a try by deleting the pos=[220,140] parameter.
+        """
+        dpg.add_radio_button(["Radio button widget - option 1", "Radio button widget - option 2", "Radio button widget - option 3"], horizontal=False,pos=[220,140])
+    
+        # The following codes create a plot widget
+        with dpg.plot(label="Line Series", height=300, width=400):
+        # optionally create legend
+            dpg.add_plot_legend()
+            # REQUIRED: create x and y axes
+            dpg.add_plot_axis(dpg.mvXAxis, label="x")
+            dpg.add_plot_axis(dpg.mvYAxis, label="y", tag="y_axis")
+            # series belong to a y axis
+            dpg.add_line_series(sindatax, sindatay, label="0.5 + 0.5 * sin(x)", parent="y_axis") 
 
-# We define a callback function to print the sender and data of a widget
-def print_me(sender, data):
-    print(sender, data)
-# The following codes create a menu bar
-with dpg.viewport_menu_bar():
-    with dpg.menu(label="File"):
-        dpg.add_menu_item(label="Save", callback=print_me)
-        dpg.add_menu_item(label="Save As", callback=print_me)
+    # There can be as many windows as you want in a viewport, here we generate a second window
+    with dpg.window(label="Window2", width=700, height=700,pos=[700,0]):
+        dpg.add_text("Text widget")
 
-        with dpg.menu(label="Settings"):
-            dpg.add_menu_item(label="Setting 1", callback=print_me, check=True)
-            dpg.add_menu_item(label="Setting 2", callback=print_me)
+    # We define a callback function to print the sender and data of a widget
+    def print_me(sender, data):
+        print(sender, data)
+    # The following codes create a menu bar
+    with dpg.viewport_menu_bar():
+        with dpg.menu(label="File"):
+            dpg.add_menu_item(label="Save", callback=print_me)
+            dpg.add_menu_item(label="Save As", callback=print_me)
 
-    dpg.add_menu_item(label="Help", callback=print_me)
+            with dpg.menu(label="Settings"):
+                dpg.add_menu_item(label="Setting 1", callback=print_me, check=True)
+                dpg.add_menu_item(label="Setting 2", callback=print_me)
 
-    with dpg.menu(label="Widget Items"):
-        dpg.add_checkbox(label="Pick Me", callback=print_me)
-        dpg.add_button(label="Press Me", callback=print_me)
-        dpg.add_color_picker(label="Color Me", callback=print_me)
+        dpg.add_menu_item(label="Help", callback=print_me)
+
+        with dpg.menu(label="Widget Items"):
+            dpg.add_checkbox(label="Pick Me", callback=print_me)
+            dpg.add_button(label="Press Me", callback=print_me)
+            dpg.add_color_picker(label="Color Me", callback=print_me)
 
 
-dpg.setup_dearpygui()
-# The viewport is shown using the show_viewport() function.
-dpg.show_viewport()
-#  The render loop is completely handled by the start_dearpygui() command.
-dpg.start_dearpygui()
-# All dearpygui apps end with destroy_context()
-dpg.destroy_context()
+    dpg.setup_dearpygui()
+    # The viewport is shown using the show_viewport() function.
+    dpg.show_viewport()
+    #  The render loop is completely handled by the start_dearpygui() command.
+    dpg.start_dearpygui()
+    # All dearpygui apps end with destroy_context()
+    dpg.destroy_context()
+
+if __name__ == "__main__":
+    # The following codes start the GUI
+    start_GUI()
--- 2_Single channel display/Python_GUI_day2_simple.py
+++ 2_Single channel display/Python_GUI_day2_simple.py
@@ -1,45 +1,62 @@
 import dearpygui.dearpygui as dpg
-from math import sin, cos
+# math is used to generate data for the plot widget
+from math import cos
 
-dpg.create_context()
+global SERIAL_TAG
 
-sindatax = []
-sindatay = []
-for i in range(0, 500):
-    sindatax.append(i / 1000)
-    sindatay.append(0.5 + 0.5 * sin(50 * i / 1000))
-
+# The following function is used to generate and update the plot widget
 def update_series(j):
-
     cosdatax = []
     cosdatay = []
-    print(j)
     for i in range(0, 500):
         cosdatax.append(i / 1000)
         cosdatay.append(0.5 + 0.5 * cos(50 * (i+j) / 1000))
-    dpg.set_value('series_tag', [cosdatax, cosdatay])
-    dpg.set_item_label('series_tag', "0.5 + 0.5 * cos(x)")
+    """
+    Pay attention
+    When you plot data without GUI, you can use plt.plot(data)
+    When you plot data with GUI, you need to use dpg.set_value() to update the data, the data will be shown
+    in the plot widget with the tag "series_tag", this tag is assigned to the plot widget at line 41.
+    """
+    dpg.set_value(SERIAL_TAG, [cosdatax, cosdatay])
+    dpg.set_item_label(SERIAL_TAG, "0.5 + 0.5 * cos(x)")
 
-with dpg.window(label="Window1", tag="win"):
-    # create plot
-    with dpg.plot(label="Line Series", height=300, width=600):
-        # optionally create legend
-        dpg.add_plot_legend()
+# The following function is used to start the GUI
+def start_GUI():
+    dpg.create_context()
+    with dpg.window(label="Window1", tag="win"):
+        # create plot
+        with dpg.plot(label="Line Series", height=300, width=600):
+            # optionally create legend
+            dpg.add_plot_legend()
 
-        # REQUIRED: create x and y axes
-        dpg.add_plot_axis(dpg.mvXAxis, label="x")
-        dpg.add_plot_axis(dpg.mvYAxis, label="y", tag="y_axis")
+            # REQUIRED: create x and y axes
+            dpg.add_plot_axis(dpg.mvXAxis, label="x")
+            dpg.add_plot_axis(dpg.mvYAxis, label="y", tag="y_axis")
 
-        # series belong to a y axis
-        dpg.add_line_series(sindatax, sindatay, parent="y_axis", tag="series_tag")
+            # series belong to a y axis
+            # We use the following codes to generate data for initialize plotting
+            cosdatax = []
+            cosdatay = []
+            for i in range(0, 500):
+                cosdatax.append(i / 1000)
+                cosdatay.append(0.5 + 0.5 * cos(50 * (i) / 1000))
+            dpg.add_line_series(cosdatax, cosdatay, parent="y_axis", tag=SERIAL_TAG)
 
+    dpg.create_viewport(title='Single channel display', width=600, height=400)
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    i = 0
+    """
+    Here we change dpg.start_dearpygui() to while dpg.is_dearpygui_running() and dpg.render_dearpygui_frame()
+    This is because we want to update the plot widget in a loop, if we use dpg.start_dearpygui(), the GUI will
+    be frozen and the plot widget will not be updated.
+    """
+    while dpg.is_dearpygui_running():
+        i = i + 1
+        update_series(i)
+        dpg.render_dearpygui_frame()
+    dpg.destroy_context()
 
-dpg.create_viewport(title='Single channel display', width=600, height=400)
-dpg.setup_dearpygui()
-dpg.show_viewport()
-i = 0
-while dpg.is_dearpygui_running():
-    i = i + 1
-    update_series(i)
-    dpg.render_dearpygui_frame()
-dpg.destroy_context()
+if __name__ == "__main__":
+    SERIAL_TAG = "series_tag"
+    start_GUI()
